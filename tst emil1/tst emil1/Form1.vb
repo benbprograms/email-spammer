2adbdd37b21abec37911f768675e884cc7c2601e
benbprograms <33466059+benbprograms@users.noreply.github.com>
2017-12-17 18:51:11 +0000

    Add files via upload

---
+++ tst emil1/tst emil1/Form1.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Net.Mail
+Imports System.Net
+Public Class Form1
+    Public mail As New MailMessage
+    Public smtp As New SmtpClient
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim ToAddr, FromAddr As String
+        'smtp setup
+        smtp.UseDefaultCredentials = False
+        smtp.Credentials = New Net.NetworkCredential("covers.ben.blakemore@gmail.com", "Bensmonkey76!!")
+        smtp.Port = 587
+        smtp.EnableSsl = True
+        smtp.Host = "smtp.gmail.com"
+        'mail setup
+        ToAddr = "aliwally1@ntlworld.com" ' who the email is to '
+        FromAddr = "ben.blakemore@aol.com" ' who the email i'
+        mail.From = New MailAddress(FromAddr)
+        mail.To.Add(ToAddr)
+        mail.Subject = "email sending..."
+        mail.IsBodyHtml = False
+        mail.Body = "hello" & Environment.NewLine & "this is a test" & Environment.NewLine & "ignore this!"
+        Dim emailSent As Integer = 0
+        'send the email
+        Do Until emailSent = 100
+            smtp.Send(mail)
+            emailSent += 1
+            Label1.Text = "emails sent " & emailSent
+        Loop
+    End Sub
+End Class
